3bc460276bd972eef8346bea4f4021bc6a305207
Yehonatan Elentok <yehonatan.e@solaredge.com>
2022-02-06 19:52:16 +0200

    [Lib,Test] Fix credit card insert test

--- test/test_credit-card-parser.py
+++ test/test_credit-card-parser.py
@@ -3,6 +3,11 @@ import pandas as pd
 from lib.credit_card_handler import CreditCardMaxXlsxParser
 def test_credit_card_parser():
     df = CreditCardMaxXlsxParser.xlsx("test/max-credit-card_test.xlsx")
-    print("\n",df)
-
-
+    data={
+            "Description": ["Work", "Do", "Something", "fun fun", "English Line", "AMZN  AMZN.COMBILL US", "AMZN  AMZN.COM/BILL US", "ALIEXPRESS", "GOOGLE"],
+            "Data": ["21-03-2021", "01-12-2021", "04-01-2022", "06-01-2022", "06-01-2022", "15-12-2021", "16-12-2021", "16-12-2021", "27-12-2021"],
+            "Amount": [200, 201, 202, 203, 203, 600, 601, 602, 603]
+            }
+    df_expected = pd.DataFrame(data=data)
+    df_expected.set_index(["Data", "Amount"], inplace=True)
+    assert df_expected.equals(df_expected)
